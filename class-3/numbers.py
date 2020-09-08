cc9770439dadc92c40e4a84e56e2571940469759
rafaelfs96 <rafaelfs96@outlook.com>
2020-09-08 20:39:19 -0300

    change quotes

--- class-3/numbers.py
+++ class-3/numbers.py
@@ -4,10 +4,10 @@ num2 = 2
 print(num1 + num2)
 print(num1 ** num2)
 
-print('\nPrint numbers with strings using "f"')
+print('\nPrint numbers with strings using \'f\'')
 print(f'{num1} plus {num2} is equals to {num1 + num2} \n')
 
-print('Or print numbers with strings converting the numbers into string using "str(number)"')
+print('Or print numbers with strings converting the numbers into string using \'str(number)\'')
 print(str(num1) + ' plus ' + str(num2) + ' is equals to ' + str(num1 + num2) + '\n')
 
 # Converting strings into numbers using 'int(string)' or 'float(string)'
